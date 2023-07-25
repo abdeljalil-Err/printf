8eceddff4f82b16160e21756dbfc5daf3ee540ff
ihssane <errajy.ihssane@gmail.com>
2023-07-25 23:07:17 +0100

    4nd Editing

--- _printstring.c
+++ _printstring.c
@@ -10,11 +10,13 @@
 int _printstring(va_list args)
 {
 	/*variables declaration section :*/
-	char *string_temp;
+	char *string_temp, *str_nu = "(null)";
 	int i = 0;
 
 	/*The code section :*/
 	string_temp = va_arg(args, char *);
+	if (string_temp == NULL)
+	return (write(1, str_nu, 6));
 	while (string_temp[i])
 	{
 	_putchar(string_temp[i]);
